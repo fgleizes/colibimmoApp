45748b77d732cde0a1a73124d0b99d079adc8d92
fjquen <fjquen@gmail.com>
2022-04-25 11:22:46 +0200

    changer ProjectAPI pour projectAPI

--- API/ProjectAPI.js
+++ API/ProjectAPI.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { View, Image,Text, StyleSheet,ScrollView } from "react-native";
-import { getProfile } from '../api/userAPI';
+import { getProfile } from './userAPI';
 
 // API URL FOR USER ROUTES
 const API_URL = "http://api.colibimmo.cda.ve.manusien-ecolelamanu.fr/public/"
@@ -18,10 +18,6 @@ export const getProjectsAchat = (token) => {
         console.log(error.response)
     })
 }
-<<<<<<< HEAD
-
-
-=======
  
 
 export const getProjectsVentes = (token) => {
@@ -86,4 +82,3 @@ export const getMainImageProject = (id_Project) => {
             console.log(error.response)
         })
 }
->>>>>>> 89ff8dae7244f73dee32480bd74c191302fea645
